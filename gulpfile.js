6e32b7be3e964b055eda51e3ced7aca6417d2a3d
nearbycoder <nearbycoder@gmail.com>
2016-01-29 22:01:33 -0600

    Add compiled and minified css
    Added minimized css to be used for production.

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,17 @@
     var gulp = r("gulp");
     var livereload = require('gulp-livereload');
     var webserver = require('gulp-webserver');
+    var cssnano = require('gulp-cssnano');
     var concat = require('gulp-concat');
+    var rename = require("gulp-rename");
 
     gulp.task("scss", function () {
         gulp.src(
-            "scss/main.scss"
+            "scss/flexer.scss"
         ).pipe(scss(
             {"bundleExec": false}
         )).pipe(gulp.dest("./src/css"))
+        	.pipe(gulp.dest("./dist/css"))
         .pipe(livereload());
     });
     gulp.task('webserver', function() {
@@ -22,12 +25,6 @@
 		      port: 8080
 		    }));
 		});
-		gulp.task('scripts', function() {
-		  return gulp.src('./js/*.js')
-		    .pipe(concat('main.js'))
-		    .pipe(gulp.dest('./src/js'))
-		    .pipe(livereload());
-		});
 
 		gulp.task('html', function() {
 		    return gulp.src([
@@ -36,12 +33,24 @@
 		    .pipe(livereload());
 		});
 
+		gulp.task('minify', function() {
+    return gulp.src('./dist/css/flexer.css')
+        .pipe(cssnano())
+        .pipe(rename("flexer.min.css"))
+        .pipe(gulp.dest('./dist/css/'));
+		});
+
+		gulp.task('dist', function() {
+    return gulp.src('./scss/**/**.*')
+        .pipe(gulp.dest('./dist/scss/'));
+		});
+
 		gulp.task('watch', function() {
 			livereload.listen();
 			gulp.watch('src/*.html', ['html']);
 		  gulp.watch('scss/**/*.scss', ['scss']);
-		  gulp.watch('js/*.js', ['scripts']);
 		});
  
     gulp.task('default', ['webserver', 'watch']);
+    gulp.task('compile', ['scss', 'minify', 'dist']);
 }(require));
